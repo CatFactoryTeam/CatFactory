56a3c1d9c1ed763eb89550c47e1f41c517882929
Brice Thomas <brice.p.thomas@gmail.com>
2014-07-08 21:52:56 +0200

    Remove useless files

--- js/cat.js
+++ js/cat.js
@@ -36,7 +36,7 @@ function new_start() {
 //@ http://jsfromhell.com/array/shuffle [v1.0]
 function shuffle(o) { //v1.0
 	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-		return o;
+	return o;
 };
 
 document.onkeydown = function(ev) {
