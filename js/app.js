c2a347b9372fb10df521aa41d372e09d4c200510
Brice Thomas <brice.p.thomas@gmail.com>
2015-03-07 11:39:17 +0100

    Manage fresh re-start

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ app.controller('Ctrl', function($scope, $http) {
 
   // By default put a local waiting gif
   $scope.cats = [$scope.waitingGif];
+  $scope.used = [];
   $scope.restApi = "http://localhost:9000";
 
   /**
@@ -24,7 +25,16 @@ app.controller('Ctrl', function($scope, $http) {
    * Meow ! Call the next cat
    */
   $scope.meow = function() {
-    $scope.cats.shift();
+    if ($scope.cats.length == 0) $scope.freshStart();
+    $scope.used.push($scope.cats.shift());
+  }
+
+  /**
+   * A fresh start, when all cats have been seen
+   */
+  $scope.freshStart = function() {
+    $scope.cats = shuffle($scope.used);
+    $scope.used = [];
   }
 
   /**
@@ -35,5 +45,12 @@ app.controller('Ctrl', function($scope, $http) {
   $scope.shuffle = function(o) {
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
+  };
+
+  /**
+   * Meow when pressing 'n'
+   */
+  $scope.onKeyDown = function(ev) {
+    if (ev.keyCode == 78) $scope.meow();
   };
 });
